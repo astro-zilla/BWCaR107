00a8d9a65fa4bb1caa073749bde658baffa69f10
rg638 <rg638@cam.ac.uk>
2021-10-20 10:29:11 +0100

    dumb

--- Daedalus/datahandling.py
+++ Daedalus/datahandling.py
@@ -7,13 +7,13 @@ def main():
         data = f.readlines()
     data = [float(item.strip('\n')) for item in data]
 
-
+    for d in data:
+        print(Smoother.process(d))
 
 
 
 if __name__ == "__main__":
     main()
 
-    for d in data:
-        print Smoother.process(d)
+
 
